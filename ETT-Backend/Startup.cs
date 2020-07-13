1705b48924a3d1ad168bbc80b9c30ebf28418232
Daniel Moon <daniel_moon16@yahoo.com>
2020-07-13 15:59:28 -0400

    Remove semicolan

--- ETT-Backend/Startup.cs
+++ ETT-Backend/Startup.cs
@@ -30,7 +30,7 @@ namespace ETT_Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers(); ;
+            services.AddControllers();
             services.AddSingleton<IEmployeeService, EmployeeService>();
             services.AddCors(opt =>
             {
