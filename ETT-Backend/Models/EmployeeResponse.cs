757deaa9517de4e72d88626e9f2ad36dc99664e3
Jordan <jordan.otrembiak@callibrity.com>
2020-08-05 11:36:58 -0400

    Removed LastUpdatedAt.cs and placed that logic within EmployeeResponse.cs

--- ETT-Backend/Models/EmployeeResponse.cs
+++ ETT-Backend/Models/EmployeeResponse.cs
@@ -14,15 +14,15 @@ namespace ETT_Backend.Models
         [JsonProperty("growth")]
         public GrowthHours Growth;
 
-        [JsonProperty("dbUpdate")]
-        public LastUpdatedAt DBUpdate;
+        [JsonProperty("updatedAt")]
+        public string UpdatedAt;
 
         public EmployeeResponse(Employee emp)
         {
             EmployeeId = emp.EmployeeNumber;
             Billable = new BillableHours(emp.CurrentBillableHours, emp.BillableTargetToDate, emp.YearlyBillableTargetHours);
             Growth = new GrowthHours(emp.CurrentTrainingHours, emp.TargetTrainingHours - emp.CurrentTrainingHours, emp.TargetTrainingHours);
-            DBUpdate = new LastUpdatedAt(emp.UpdatedAt);
+            UpdatedAt = emp.UpdatedAt.ToString("MM/dd/yyyy HH:mm:ss");
         }
 
         public EmployeeResponse()
